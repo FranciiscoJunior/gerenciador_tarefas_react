84fbab45852cb68e0120ed62b4d56f482ec5674c
Francisco Júnior <franciscoamj1001@gmail.com>
2025-07-24 15:27:44 -0300

    Atualizando correções

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,6 @@
-import { Button } from "flowbite-react";
-import { ChevronRightIcon, TrashIcon} from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 
 function Tasks ({tasks, onTaskClick, onDeleteTaskClick}) {
     const navigate = useNavigate()
@@ -16,11 +16,13 @@ function Tasks ({tasks, onTaskClick, onDeleteTaskClick}) {
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {tasks.map((task) => (
                 <li key={task.id} className="flex gap-2">
-
+                    
                     <button onClick={() => onTaskClick(task.id)}
-                        className={`bg-slate-400 text-left w-full text-white p-2 rounded-md
+                        className={`bg-slate-400 flex items-center gap-2 text-left w-full text-white p-2 rounded-md
                             ${task.isCompleted && "line-through"
                         }`}>
+
+                            {task.isCompleted && <CheckIcon/>}
                         {task.title}
                     </button>
 
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -7,16 +7,13 @@ function TaskPage() {
     const [searchParams] = useSearchParams();
     const title = searchParams.get("title");
     const description = searchParams.get("description");
-    function onBackClick() {
-        navigate(-1);
-    }
 
     return (
         <div className="h-screen w-screen bg-slate-500 p-6">
-            <div className="w-[500px] space-y-4">
+            <div className="w-[500px] mx-auto space-y-4">
                 <div className="flex justify-center relative mb-6">
                     <button
-                    onClick={onBackClick}
+                    onClick={() => navigate(-1) }
                     className="absolute left-0 top-0 bottom-0 text-slate-100">
                         <ChevronRightIcon/>
                     </button>
